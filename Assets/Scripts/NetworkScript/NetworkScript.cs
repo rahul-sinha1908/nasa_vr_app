b0c086b51afd934d7b2e158a08cee005fb2f56d6
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-29 11:03:59 +0530

    Made the Universe

---
+++ Assets/Scripts/NetworkScript/NetworkScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkView))]
+public class NetworkScript : MonoBehaviour {
+
+	public NetworkView netView;
+	// Use this for initialization
+	void Start () {
+		netView=GetComponent<NetworkView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
