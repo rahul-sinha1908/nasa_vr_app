64d42b95683d51335e88f7171a1a5af3c3c96d18
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-29 16:26:34 +0530

    Saved the project.

--- Assets/Scripts/MyScripts/MyPlayerScript.cs
+++ Assets/Scripts/MyScripts/MyPlayerScript.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using MyGame;
 
 [RequireComponent(typeof(NetworkView))]
+[RequireComponent(typeof(CharacterController))]
 public class MyPlayerScript : MonoBehaviour {
 	
 	public LayerMask collidingLayer;
 	private NetworkView netView;
-
+	private CharacterController controller;
 	// Use this for initialization
 	void Start () {
 		netView=GetComponent<NetworkView>();
+		controller=GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -23,7 +25,18 @@ public class MyPlayerScript : MonoBehaviour {
 		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 		RaycastHit outP;
 		if(Physics.Raycast(ray, out outP, 1000, collidingLayer)){
-			
+			if(outP.collider.gameObject.layer==LayerMask.NameToLayer("Floor")){
+				movePlayer(outP.point);
+			}else if(outP.collider.gameObject.layer==LayerMask.NameToLayer("Menu")){
+
+			}else if(outP.collider.gameObject.layer==LayerMask.NameToLayer("Earth")){
+				
+			}else if(outP.collider.gameObject.layer==LayerMask.NameToLayer("")){
+				
+			}
 		}
+	}
+	private void movePlayer(Vector3 pos){
+
 	}
 }
